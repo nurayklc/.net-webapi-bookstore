a93ea7552380aa3348636b9caf887f1bb1aa4fb9
nurayklc <nuraykilic0101@gmail.com>
2023-03-01 17:58:10 +0300

    Start test

---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/CreateCommand/CreateBookCommandTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.CreateCommand
+{
+    public class CreateBookCommandTests
+    {
+
+    }
+}
---
+++ Tests/WebApi.UnitTests/TestSetup/CommanTestFixture.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.DBOperations;
+
+namespace WebApi.UnitTests.TestSetup
+{
+    public class CommanTestFixture
+    {
+        public BookStoreDbContext BookStoreDbContext { get; set; }
+        public IMapper Mapper { get; set; }
+        public CommanTestFixture()
+        {
+
+        }
+    }
+}
