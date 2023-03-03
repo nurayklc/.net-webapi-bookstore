18a8783566de144dba0bd4dee7aafc2fa179dc69
nurayklc <nuraykilic0101@gmail.com>
2023-03-03 15:18:19 +0300

    Add update book command tests

---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/UpdateCommand/UpdateBookCommandTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.BookOperations.UpdateBook;
+using WebApi.DBOperations;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.UpdateCommand
+{
+    public class UpdateBookCommandTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+
+        public UpdateBookCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.BookStoreDbContext;
+        }
+
+        [Fact]
+        public void WhenUpdateBookInputIsGiven_InvalidException_ShouldReturnError()
+        {
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            command.BookId = 10000;
+
+            FluentActions.Invoking(() => command.Handle()).Should().Throw<InvalidOperationException>()
+                .And.Message.Should().Be("Güncellenecek Kitap Bulunamadı!");
+        }
+        [Fact]
+        public void WhenUpdateBookIsGiven_Book_ShouldUpdated()
+        {
+            UpdateBookCommand command = new UpdateBookCommand(_context);
+            command.Model = new UpdateBookModel()
+            {
+                GenreId = 5,
+                Title = "Test",
+            };
+            command.BookId = 5;
+
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            var book = _context.Books.SingleOrDefault(x => x.Id == command.BookId);
+            book.Should().NotBeNull();
+            book.GenreId.Should().Be(command.Model.GenreId);
+            book.Title.Should().Be(command.Model.Title);
+        }
+    }
+}
---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/UpdateCommand/UpdateBookCommandValidatorTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.UpdateBook;
+using WebApi.DBOperations;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.UpdateCommand
+{
+    public class UpdateBookCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+
+        public UpdateBookCommandValidatorTests(CommonTestFixture fixture)
+        {
+            _context = fixture.BookStoreDbContext;
+        }
+
+        [Theory]
+        [InlineData(" ", 100)]
+        [InlineData("", 0)]
+        [InlineData(" ", 100)]
+        public void WhenInputBookTitleIsGiven_Validate_ShouldReturnError(string title, int genreId) 
+        {
+            UpdateBookCommand command = new UpdateBookCommand(null);
+            command.Model = new UpdateBookModel()
+            {
+                Title = title,
+                GenreId = genreId
+             };
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            var result = validator.Validate(command);
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenValidInputAreGiven_Validator_ShouldNotBeReturnError()
+        {
+            UpdateBookCommand command = new UpdateBookCommand(null);
+            command.Model = new UpdateBookModel()
+            {
+                Title = "Lord of the Rings",
+                GenreId = 1
+            };
+            UpdateBookCommandValidator validator = new UpdateBookCommandValidator();
+            var result = validator.Validate(command);
+            result.Errors.Count.Should().BeGreaterThanOrEqualTo(0);
+        }
+    }
+}
