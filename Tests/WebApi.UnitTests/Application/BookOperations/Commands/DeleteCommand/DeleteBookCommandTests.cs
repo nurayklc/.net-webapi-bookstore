becbc8da905406dda5991d74a463b14990ab0a10
nurayklc <nuraykilic0101@gmail.com>
2023-03-03 12:44:51 +0300

    Add delete book commad test

---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/DeleteCommand/DeleteBookCommandTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.BookOperations.DeleteBook;
+using WebApi.DBOperations;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.DeleteCommand
+{
+    public class DeleteBookCommandTests : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+
+        public DeleteBookCommandTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.BookStoreDbContext;
+        }
+
+        [Theory]
+        [InlineData(120)]
+        [InlineData(1000)]
+        [InlineData(2000)]
+        [InlineData(3000)]
+        public void WhenNotBookIsGiven_InvalidException_ShouldBeReturn(int bookId)
+        {
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            command.BookId = bookId;
+
+            FluentActions.Invoking(() => command.Handle())
+                .Should().Throw<InvalidOperationException>().And.Message.Should().Be("Kitap bulunmamaktadır!");
+        }
+        [Fact]
+        public void WhenBookIsGiven_Book_ShouldBeDeleted()
+        {
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            command.BookId = 1;
+            FluentActions.Invoking(() => command.Handle()).Invoke();
+
+            var book = _context.Books.SingleOrDefault(x => x.Id == command.BookId);
+            book.Should().BeNull();
+        }
+    }
+}
---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Commands/DeleteCommand/DeleteBookCommandValidatorTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.BookOperations.DeleteBook;
+using WebApi.DBOperations;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Commands.DeleteCommand
+{
+    public class DeleteBookCommandValidatorTests : IClassFixture<CommonTestFixture>
+    {
+        [Fact]
+        public void WhenValidBook_Validator_ShouldBeReturnError()
+        {
+            DeleteBookCommand command = new DeleteBookCommand(null);
+            command.BookId = -1;
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();   
+            var result = validator.Validate(command);
+            result.Errors.Count.Should().BeGreaterThan(0);
+        }
+
+        [Fact]
+        public void WhenValidInputAreGivenBook_Validator_ShouldNotBeError()
+        {
+            DeleteBookCommand command = new DeleteBookCommand(null);
+            command.BookId = 1;
+            DeleteBookCommandValidator validator = new DeleteBookCommandValidator();
+            var result = validator.Validate(command);
+            result.Errors.Count.Should().Be(0);
+        }
+    }
+}
