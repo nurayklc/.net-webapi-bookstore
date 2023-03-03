60566db4c9216a055722898dcbc746161709d5fd
nurayklc <nuraykilic0101@gmail.com>
2023-03-03 17:48:29 +0300

    Add get books method tests

---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Queries/GetBookDetailQueryTest/GetBookDetailQueryTests.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.BookOperations.GetBookDetail;
+using WebApi.DBOperations;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Queries.GetBookDetailQueryTest
+{
+    public class GetBookDetailQueryTests : IClassFixture<CommonTestFixture>
+    {
+
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetBookDetailQueryTests(CommonTestFixture testFixture)
+        {
+            _context = testFixture.BookStoreDbContext;
+            _mapper = testFixture.Mapper;
+        }
+
+        [Fact]
+        public void WhenValidInputsAreGiven_Book_ShouldBeReturned()
+        {
+            // arrange
+            GetBookDetailQuery query = new(_context, _mapper);
+            var BookId = query.BookId = 1;
+
+            var book = _context.Books.Include(x => x.Genre).Include(x => x.Author).Where(b => b.Id == BookId).SingleOrDefault();
+
+            // act
+            BookDetailViewModel vm = query.Handle();
+
+            // assert
+            vm.Should().NotBeNull();
+            vm.Title.Should().Be(book.Title);
+            vm.PageCount.Should().Be(book.PageCount);
+            vm.Genre.Should().Be(book.Genre.Name);
+            vm.Author.Should().Be(book.Author.Name + " " + book.Author.Surname);
+            vm.PublishDate.Should().Be(book.PublishDate.ToString("dd/MM/yyyy 00:00:00"));
+        }
+
+        [Fact]
+        public void WhenNonExistingBookIdIsGiven_InvalidOperationException_ShouldBeReturn()
+        {
+            // arrange
+            int bookId = 11;
+
+            GetBookDetailQuery query = new GetBookDetailQuery(_context, _mapper);
+            query.BookId = bookId;
+
+            // assert
+            query.Invoking(x => x.Handle())
+                 .Should().Throw<InvalidOperationException>()
+                 .And.Message.Should().Be("Kitap bulunmamaktadır!");
+        }
+    }
+}
---
+++ Tests/WebApi.UnitTests/Application/BookOperations/Queries/GetBooksQueryTest/GetBooksQueryTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.BookOperations.GetBooks;
+using WebApi.DBOperations;
+using WebApi.UnitTests.TestSetup;
+
+namespace WebApi.UnitTests.Application.BookOperations.Queries.GetBooksQueryTest
+{
+    public class GetBooksQueryTest : IClassFixture<CommonTestFixture>
+    {
+        private readonly BookStoreDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetBooksQueryTest(CommonTestFixture testFixture)
+        {
+            _mapper = testFixture.Mapper;
+            _context = testFixture.BookStoreDbContext;
+        }
+
+        [Fact]
+        public void WhenBooksIsGiven_Books_ShouldReturnError()
+        {
+            // Arrange
+            var query = new GetBooksQuery(_context, _mapper);
+
+            // Act
+            var result = query.Handle();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(3);
+
+        }
+    }
+}
