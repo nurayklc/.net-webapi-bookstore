6d98f8c458d75b2ee354603d80e835ae95c03c70
nurayklc <nuraykilic0101@gmail.com>
2023-03-10 17:54:32 +0300

    Add user entity

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,14 @@
-using System.Threading;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
 using WebApi.DBOperations;
 using System.Reflection;
 using WebApi.Middlewares;
 using WebApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
+ConfigurationManager Configuration = builder.Configuration;
 
 // Add services to the container.
 //builder.Services.AddDbContext(options => options.UseInMemoryDatabase(databaseName:"BookStoreDB"));
@@ -15,6 +18,21 @@ builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
 builder.Services.AddSingleton<ILoggerService, ConsoleLogger>();
 builder.Services.AddSingleton<ILoggerService, DBLogger>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Token:Issuer"],
+        ValidAudience = builder.Configuration["Token:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -35,6 +53,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
 
 app.UseCustomExceptionMiddle();
 
